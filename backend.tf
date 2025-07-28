e1e7a475ed850274db0f1858af2b862059b6d44d
Biswajit Samal <biswajitsamalsibu03@gmail.com>
2025-07-28 17:15:47 +0530

    Setup terraform CI/CD with github actions

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-pipeline-artifacts-bucket"
     key            = "state/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-south-1"
     dynamodb_table = "terraform-locks" # optional but recommended
     encrypt        = true
   }
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/Biswajits-Hub/terraform
+module github.com/Samal-Biswajit/Terraform
 
 go 1.24.5
 
