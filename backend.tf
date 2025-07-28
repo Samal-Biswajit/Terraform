dddf332d594a48a8ba2529de996dc674e0daac04
Biswajit Samal <biswajitsamalsibu03@gmail.com>
2025-07-28 17:31:32 +0530

    fix: updated backend key and region for S3 backend

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-pipeline-artifacts-bucket"
-    key            = "state/terraform.tfstate"
+    bucket         = "my-artifact-bucket-biswajit"
+    key            = "terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-locks" # optional but recommended
     encrypt        = true
   }
 }
